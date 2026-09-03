f1e68595f243f5f117fde92c21a2b89bc91fafa1
Akshay Mone <mone.akshay@gmail.com>
2026-09-03 04:16:53 +0000

    chore: add run_server.bat to debug MCP server

---
+++ run_server.bat
@@ -0,0 +1,3 @@
+@echo off
+set PYTHONIOENCODING=utf-8
+"D:\work_dsi\Projects\korea\git\mcp_tools\.venv\Scripts\python.exe" -u "D:\work_dsi\Projects\korea\git\mcp_tools\server.py" 2> "D:\work_dsi\Projects\korea\git\mcp_tools\mcp_error.log"
